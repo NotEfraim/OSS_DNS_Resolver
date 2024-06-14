bcaf1dc946380cb504de96a458e5dd4d5d88aff4
Kirito <efraimcanilang404@gmail.com>
2024-06-14 17:35:36 +0800

    Pom update 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -126,7 +126,7 @@ publishing {
 
             groupId = "com.github.NotEfraim"
             artifactId = "OSS_DNS_Resolver"
-            version = "1.3"
+            version = "1.4"
 
             pom {
                 name = "OSS DNS Resolver"
