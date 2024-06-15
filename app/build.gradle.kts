0fc6a33f82532eb26c5d94d5572fa0ede0c8f00e
Kirito <efraimcanilang404@gmail.com>
2024-06-15 10:39:23 +0800

    Pom update 1.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -125,7 +125,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.NotEfraim"
             artifactId = "OSS_DNS_Resolver"
-            version = "1.12"
+            version = "1.13"
 
             pom {
                 name.set("OSS DNS Resolver")
